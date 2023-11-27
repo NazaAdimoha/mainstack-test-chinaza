f58b4d29b413cd73bc7688aca0d44a055e8b013d
nazaadimoha <nazaadimoha98@gmail.com>
2023-11-27 17:44:47 +0100

    nav almost done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "../../public/mainstack-logo.svg";
 import Menu from "../../public/menu.svg";
+import Chat from "../../public/chat.svg";
+import Notification from "../../public/notifications.svg";
 import { navData } from "@/data/navData";
 
 const Navbar = () => {
@@ -17,7 +19,7 @@ const Navbar = () => {
           {navData.map((item, index) => (
             <div className="rounded-full mx-4" style={{ backgroundColor: `${item.bgColor}`, color: `${item.color}`, padding: "8px 18px 8px 14px" }} key={index}>
               <Link
-                className="hidden md:flex items-center justify-center h-full"
+                className="hidden md:flex md:items-center md:justify-center h-full"
                 href={item.href}
               >
                 <Image src={item.img} alt="logo" width={25} height={25} />
@@ -27,14 +29,16 @@ const Navbar = () => {
           ))}
 
           {/* Mobile hambugger */}
-          <div className="hidden md:flex items-center justify-center h-full ">
+          <div className="md:flex items-center justify-center h-full gap-8">
             <Link href="/">
-                <Image src={Menu} alt="logo" width={25} height={25} />
+                <Image src={Notification} alt="logo" width={25} height={25} />
             </Link>
             <Link href="/">
-                <Image src={Menu} alt="logo" width={25} height={25} />
+                <Image src={Chat} alt="logo" width={25} height={25} />
             </Link>
-            <button className="focus:outline-none">
+            
+            <button className="focus:outline-none flex justify-center items-center bg-gray-400 px-2 rounded-full py-2 gap-4">
+            <p className="bg-gray-600 w-8 h-8 flex justify-center items-center text-color text-center text-sm font-semibold rounded-full">OJ</p>
               <Image src={Menu} alt="logo" width={25} height={25} />
             </button>
           </div>
