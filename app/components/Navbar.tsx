6a85c65f7ee4dfacc2c23f03263caa13a56f5d4b
nazaadimoha <nazaadimoha98@gmail.com>
2023-11-27 17:21:59 +0100

    set up nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../public/mainstack-logo.svg";
+import Menu from "../../public/menu.svg";
+import { navData } from "@/data/navData";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Nav</div>
-    </div>
-  )
-}
+    <nav className="bg-white shadow-xl z-[100] w-full h-16 rounded-full">
+      <div className="container px-4 py-2 mx-auto">
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <Image src={Logo} alt="logo" width={25} height={25} />
+          </Link>
 
-export default Navbar
+          {/* mid nav items */}
+          {navData.map((item, index) => (
+            <div className="rounded-full mx-4" style={{ backgroundColor: `${item.bgColor}`, color: `${item.color}`, padding: "8px 18px 8px 14px" }} key={index}>
+              <Link
+                className="hidden md:flex items-center justify-center h-full"
+                href={item.href}
+              >
+                <Image src={item.img} alt="logo" width={25} height={25} />
+                <p className="ml-2">{item.title}</p>
+              </Link>
+            </div>
+          ))}
+
+          {/* Mobile hambugger */}
+          <div className="hidden md:flex items-center justify-center h-full ">
+            <Link href="/">
+                <Image src={Menu} alt="logo" width={25} height={25} />
+            </Link>
+            <Link href="/">
+                <Image src={Menu} alt="logo" width={25} height={25} />
+            </Link>
+            <button className="focus:outline-none">
+              <Image src={Menu} alt="logo" width={25} height={25} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ data/navData.tsx
@@ -0,0 +1,37 @@
+import Home from "../public/home.svg";
+import Payment from "../public/payments.svg";
+import Chart from "../public/insert_chart.svg";
+import Group from "../public/group.svg";
+
+export interface NavData {
+  title: string;
+  href: string;
+  img: any;
+  bgColor?: string;
+  color?: string;
+}
+
+export const navData: NavData[] = [
+  {
+    title: "Home",
+    href: "/",
+    img: Home,
+  },
+  {
+    title: "Analytics",
+    href: "/",
+    img: Chart,
+  },
+  {
+    title: "Revenue",
+    href: "/",
+    img: Payment,
+    bgColor: "#131316",
+    color: "#fff",
+  },
+  {
+    title: "CRM",
+    href: "/",
+    img: Group,
+  },
+];
